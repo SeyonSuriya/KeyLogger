393cf294247c14caa3a04fc8d101b1ff1d0f6984
Seyon <suriyaseyon6@gmail.com>
2023-08-14 07:29:16 +0530

    created ClipboardMonitor class inside clipboard_grap.py

--- Project/clipboard_grap.py
+++ Project/clipboard_grap.py
@@ -2,30 +2,28 @@ import pyperclip
 import time
 from datetime import datetime
 
-# Initialize the previous clipboard content
-previous_clipboard_content = pyperclip.paste()
+class ClipboardMonitor:
+    def __init__(self, interval=1):
+        self.interval = interval
+        self.previous_clipboard_content = pyperclip.paste()
 
-def save_clipboard_content(content):
-    # Get current date and time
-    now = datetime.now()
-    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    def save_clipboard_content(self, content):
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+        content_to_save = f"{now.date()} - {now.time()} - \n{content}\n"
 
-    # Prepare content with date, time, and grabbed content
-    content_to_save = f"{now.date()} - {now.time()} - \n{content}\n"
+        with open("clipboard_grap.txt", "a") as file:
+            file.write(content_to_save)
 
-    # Save content to the "clipboard.txt" file
-    with open("clipboard_grap.txt", "a") as file:
-        file.write(content_to_save)
-
-print("Clipboard monitoring started. Press 'Ctrl+C' to stop.")
-
-try:
-    while True:
-        clipboard_content = pyperclip.paste()
-        if clipboard_content != previous_clipboard_content:
-            previous_clipboard_content = clipboard_content
-            save_clipboard_content(clipboard_content)
-            print("New clipboard content saved.")
-        time.sleep(1)  # Adjust the interval as needed
-except KeyboardInterrupt:
-    print("Clipboard monitoring stopped.")
+    def start_monitoring(self):
+        print("Clipboard monitoring started. Press 'Ctrl+C' to stop.")
+        try:
+            while True:
+                clipboard_content = pyperclip.paste()
+                if clipboard_content != self.previous_clipboard_content:
+                    self.previous_clipboard_content = clipboard_content
+                    self.save_clipboard_content(clipboard_content)
+                    print("New clipboard content saved.")
+                time.sleep(self.interval)
+        except KeyboardInterrupt:
+            print("Clipboard monitoring stopped.")
