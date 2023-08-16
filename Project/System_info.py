732c0e90b99d9e6239d3eb9c88b204aa93500001
Seyon <suriyaseyon6@gmail.com>
2023-08-16 07:56:24 +0530

    created SystemInfoRecorder class inside System_info.py

--- Project/System_info.py
+++ Project/System_info.py
@@ -5,82 +5,85 @@ import socket
 import psutil
 import tkinter as tk
 
-# Get Username
-username = getpass.getuser()
+class SystemInfoRecorder:
+    def __init__(self, output_filename="system_info.txt"):
+        self.output_filename = output_filename
+        self.root = tk.Tk()
 
-# Get Home Directory
-home_directory = os.path.expanduser("~")
+    def get_username(self):
+        return getpass.getuser()
 
-# Get System Information
-system_info = platform.uname()
+    def get_home_directory(self):
+        return os.path.expanduser("~")
 
-# Get Current Working Directory
-current_directory = os.getcwd()
+    def get_system_info(self):
+        return platform.uname()
 
-# Get Environment Variables
-user_profile = os.getenv("USERPROFILE")  # Windows
-user_home = os.getenv("HOME")  # Unix-like systems
-user_name = os.getenv("USERNAME")  # Windows
+    def get_current_directory(self):
+        return os.getcwd()
 
-# Get Host Name
-host_name = socket.gethostname()
+    def get_user_profile(self):
+        return os.getenv("USERPROFILE")  # Windows
 
-# Get Hardware details
-processor = platform.processor()
-virtual_memory = psutil.virtual_memory()
-total_memory = virtual_memory.total / 1024 ** 3
-available_memory = virtual_memory.available / 1024 ** 3
+    def get_user_home(self):
+        return os.getenv("HOME")  # Unix-like systems
 
-# Get disk partitions
-disk_partitions = psutil.disk_partitions()
+    def get_user_name(self):
+        return os.getenv("USERNAME")  # Windows
 
-# Display Information
-root = tk.Tk()
+    def get_host_name(self):
+        return socket.gethostname()
 
-# Get screen size (dimensions)
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+    def get_hardware_details(self):
+        processor = platform.processor()
+        virtual_memory = psutil.virtual_memory()
+        total_memory = virtual_memory.total / 1024 ** 3
+        available_memory = virtual_memory.available / 1024 ** 3
+        return processor, total_memory, available_memory
 
-# Get screen resolution (DPI)
-screen_resolution = root.winfo_fpixels('1i')  # Pixels per inch
+    def get_disk_partitions(self):
+        return psutil.disk_partitions()
 
-# Write Details to File
-with open("system_info.txt", "w") as file:
-    file.write(f"Username: {username}\n")
-    file.write(f"Home Directory: {home_directory}\n")
-    file.write(f"System Information: {system_info}\n")
-    file.write(f"Current Working Directory: {current_directory}\n")
-    file.write(f"User Profile: {user_profile}\n")
-    file.write(f"User Home: {user_home}\n")
-    file.write(f"User Name: {user_name}\n")
-    file.write(f"Host Name: {host_name}\n")
-    file.write("\n")
-    file.write(f"System Hardware\n")
-    file.write(f" Processor: {processor}\n")
-    file.write(f" Total Memory: {total_memory:.2f} GB\n")
-    file.write(f" Available Memory: {available_memory:.2f} GB\n")
+    def write_system_info_to_file(self):
+        with open(self.output_filename, "w") as file:
+            file.write(f"Username: {self.get_username()}\n")
+            file.write(f"Home Directory: {self.get_home_directory()}\n")
+            file.write(f"System Information: {self.get_system_info()}\n")
+            file.write(f"Current Working Directory: {self.get_current_directory()}\n")
+            file.write(f"User Profile: {self.get_user_profile()}\n")
+            file.write(f"User Home: {self.get_user_home()}\n")
+            file.write(f"User Name: {self.get_user_name()}\n")
+            file.write(f"Host Name: {self.get_host_name()}\n")
+            file.write("\n")
+            file.write(f"System Hardware\n")
+            processor, total_memory, available_memory = self.get_hardware_details()
+            file.write(f" Processor: {processor}\n")
+            file.write(f" Total Memory: {total_memory:.2f} GB\n")
+            file.write(f" Available Memory: {available_memory:.2f} GB\n")
 
-    file.write("\nDisk Information:\n")
-    for partition in disk_partitions:
-        try:
-            partition_info = psutil.disk_usage(partition.mountpoint)
-            total_size_gb = partition_info.total / 1024 ** 3
-            used_space_gb = partition_info.used / 1024 ** 3
-            free_space_gb = partition_info.free / 1024 ** 3
-            file.write(f" Partition: {partition.device}\n")
-            file.write(f"   Mountpoint: {partition.mountpoint}\n")
-            file.write(f"   Total Size: {total_size_gb:.2f} GB\n")
-            file.write(f"   Used Space: {used_space_gb:.2f} GB\n")
-            file.write(f"   Free Space: {free_space_gb:.2f} GB\n")
-            file.write(f"   Usage Percentage: {partition_info.percent}%\n\n")
-        except PermissionError:
-            file.write(f"Partition: {partition.device} - Not accessible\n\n")
-            continue
+            file.write("\nDisk Information:\n")
+            for partition in self.get_disk_partitions():
+                try:
+                    partition_info = psutil.disk_usage(partition.mountpoint)
+                    total_size_gb = partition_info.total / 1024 ** 3
+                    used_space_gb = partition_info.used / 1024 ** 3
+                    free_space_gb = partition_info.free / 1024 ** 3
+                    file.write(f" Partition: {partition.device}\n")
+                    file.write(f"   Mountpoint: {partition.mountpoint}\n")
+                    file.write(f"   Total Size: {total_size_gb:.2f} GB\n")
+                    file.write(f"   Used Space: {used_space_gb:.2f} GB\n")
+                    file.write(f"   Free Space: {free_space_gb:.2f} GB\n")
+                    file.write(f"   Usage Percentage: {partition_info.percent}%\n\n")
+                except PermissionError:
+                    file.write(f"Partition: {partition.device} - Not accessible\n\n")
+                    continue
 
-    file.write(f"\nScreen Information:\n")
-    file.write(f" Screen Size: {screen_width} x {screen_height} pixels\n")
-    file.write(f" Screen Resolution: {screen_resolution:.2f} DPI\n")
+            screen_width = self.root.winfo_screenwidth()
+            screen_height = self.root.winfo_screenheight()
+            screen_resolution = self.root.winfo_fpixels('1i')  # Pixels per inch
+            file.write(f"\nScreen Information:\n")
+            file.write(f" Screen Size: {screen_width} x {screen_height} pixels\n")
+            file.write(f" Screen Resolution: {screen_resolution:.2f} DPI\n")
 
-root.quit()
+        print(f"System information written to {self.output_filename}")
 
-print("System information written to system_info.txt")
