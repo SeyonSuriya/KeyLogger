30e8620d5d7a01653559fe18f8d6e9f729d3bfe3
Seyon <suriyaseyon6@gmail.com>
2023-08-17 07:39:10 +0530

    created condition to create "Logs" folder where all gathered details are going to be stored.

---
+++ Project/KLOG.py
@@ -0,0 +1,13 @@
+import os
+
+if __name__ == "__main__":
+
+    # creating folder "Logs" to store all the data gathered from target
+    if not os.path.exists("../Logs"):
+        relative_path = "../Logs"
+
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+        log_folder_path = os.path.join(script_directory, relative_path)
+
+        os.makedirs(log_folder_path, exist_ok=True)
+
